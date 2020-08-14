5503130651743ce24c6bc884b4644957414566d3
BuildTools <unconfigured@null.spigotmc.org>
2020-08-14 18:45:09 +0200

    echo command added

---
+++ cogs/echo.py
@@ -0,0 +1,13 @@
+import discord 
+from discord.ext import commands     
+
+class Zakuren(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot 
+        
+    @commands.command()
+    async def echo(self, ctx, msg):
+        await ctx.send(f"{msg}")
+        
+def setup(bot):
+    bot.add_cog(Zakuren(bot))
