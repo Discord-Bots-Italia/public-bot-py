9808de99f5f82c30ea82ccfd9421ae5210cd7638
ssebastianoo <sebastianogirotto05@gmail.com>
2020-08-14 17:49:10 +0200

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord 
+from discord.ext import commands 
+import os 
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path = ".env")
+
+bot = commands.Bot(command_prefix = commands.when_mentioned_or("py "), case_insensitive = True)
+
+@bot.event
+async def on_ready():
+    print("ready as", bot.user)
+
+for a in os.listdir("./cogs"):
+    if a.endswith(".py"):
+        bot.load_extension(f"cogs.{a[:-3]}")
+
+token = os.environ.get("token")
+bot.run(token)
