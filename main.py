5d1bf899808750c4bfafa9c96b891f4c5d229444
ssebastianoo <sebastianogirotto05@gmail.com>
2020-08-14 17:58:57 +0200

    readme

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path = ".env")
 
 bot = commands.Bot(command_prefix = commands.when_mentioned_or("py "), case_insensitive = True)
+bot.load_extension("jishaku")
 
 @bot.event
 async def on_ready():
