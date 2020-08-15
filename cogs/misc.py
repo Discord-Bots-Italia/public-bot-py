2d8ac5f682986df86509d62902a8a62da060a958
ssebastianoo <sebastianogirotto05@gmail.com>
2020-08-15 13:07:13 +0200

    better src

--- cogs/misc.py
+++ cogs/misc.py
@@ -11,8 +11,30 @@ class Misc(commands.Cog):
         await ctx.send(f"Pong! {pong}ms:ping_pong:")
 
     @commands.command(aliases = ["src"])
-    async def source(self, ctx):
-        await ctx.send("https://github.com/Discord-Bots-Italia/public-bot-py")
+    async def source(self, ctx, *, command: str = None):
+        """Get bot source or source of a command"""
+
+        source_url = 'https://github.com/Discord-Bots-Italia/public-bot-py'
+        branch = 'master'
+
+        if command is None:
+            return await ctx.send(source_url)
+
+        else:
+            obj = self.bot.get_command(command.replace('.', ' '))
+            if obj is None:
+                return await ctx.send('Could not find command.')
+
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
+            src = obj.callback.__code__
+            module = obj.callback.__module__
+            filename = src.co_filename
+
+        lines, firstlineno = inspect.getsourcelines(src)
+
+        final_url = f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
+        await ctx.send(final_url)
         
 def setup(bot):
     bot.add_cog(Misc(bot))
