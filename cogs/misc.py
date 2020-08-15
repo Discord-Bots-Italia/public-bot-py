c245ac586547aaef89edcf0beb7bf552435e87f6
ssebastianoo <sebastianogirotto05@gmail.com>
2020-08-15 13:03:12 +0200

    source command

--- cogs/misc.py
+++ cogs/misc.py
@@ -9,6 +9,10 @@ class Misc(commands.Cog):
     async def ping(self, ctx):
         pong = round(self.bot.latency * 1000)
         await ctx.send(f"Pong! {pong}ms:ping_pong:")
+
+    @commands.command(aliases = ["src"])
+    async def source(self, ctx):
+        await ctx.send("https://github.com/Discord-Bots-Italia/public-bot-py")
         
 def setup(bot):
     bot.add_cog(Misc(bot))
