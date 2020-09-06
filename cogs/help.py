1472b5dadef35e37fc4cbec4b5ef0105336e1778
ssebastianoo <sebastianogirotto05@gmail.com>
2020-09-06 23:41:49 +0200

    minimal help command

---
+++ cogs/help.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands 
+
+class HelpCommand(commands.MinimalHelpCommand):
+    def __init__(self):
+        self.show_hidden = False
+        
+    def get_command_signature(self, command):
+        return f"{self.clean_prefix}{command.qualified_name}{command.signature}"
+
+class Help(commands.Cog):
+    def __init__(self, bot):
+        self._original_help_command = bot.help_command
+        bot.help_command = HelpCommand()
+        bot.help_command.cog = self
+
+    def cog_unload(self):
+        self.bot.help_command = self._original_help_command
+
+def setup(bot):
+    bot.add_cog(Help(bot))
